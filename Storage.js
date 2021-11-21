99b73f48b611641c091a81f1139c1b55c46ca36b
george-shammar <george.gbenle@outlook.com>
2021-11-21 23:10:00 +0000

    Add metadata

--- Storage.js
+++ Storage.js
@@ -5,9 +5,9 @@ const apiKey = process.env.API_KEY
 const client = new NFTStorage({ token: apiKey })
 
 const metadata = await client.store({
-  name: 'Pinpie',
-  description: 'Pin is not delicious beef!',
-  image: new File(['./assets/benin.jpg'], 'pinpie.jpg', { type: 'image/jpg' })
+  name: 'Benin Bronze Head',
+  description: 'The ancestral bronze head of the Benin Kingdom of West Africa.',
+  image: new File(['./assets/benin.jpg'], 'benin.jpg', { type: 'image/jpg' })
 })
 console.log(metadata.url)
 // ipfs://bafyreib4pff766vhpbxbhjbqqnsh5emeznvujayjj4z2iu533cprgbz23m/metadata.json
