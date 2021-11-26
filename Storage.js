119bd4b64f2ae01bc2f74d45c4b05c0ffcf359bb
george-shammar <george.gbenle@outlook.com>
2021-11-26 17:41:09 +0000

    Edit script

--- Storage.js
+++ Storage.js
@@ -10,4 +10,3 @@ const metadata = await client.store({
   image: new File(['./assets/benin.jpg'], 'benin.jpg', { type: 'image/jpg' })
 })
 console.log(metadata.url)
-// ipfs://bafyreib4pff766vhpbxbhjbqqnsh5emeznvujayjj4z2iu533cprgbz23m/metadata.json
